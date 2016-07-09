615a2f0c6b277e42c842c5f7629c2440300c80b2
stt <583012650@qq.com>
2016-07-09 19:48:33 +0800

    代码提交

--- Demo/src/main/java/Test.java
+++ Demo/src/main/java/Test.java
@@ -2,7 +2,28 @@
  * Created by Administrator on 2016/7/9.
  */
 public class Test {
+    private String name;
+    private int age;
     public String s(){
         return "gsad";
+    }
+    public String s2(){
+        return "gdsa";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
---
+++ Demo/src/main/java/Test2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/7/9.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+    }
+
+
+}
